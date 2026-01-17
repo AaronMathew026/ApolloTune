821d019239e0990e96c8820b5c90a6a3512e64aa
Aaron <aaron.mathew@student.manchester.ac.uk>
2026-01-17 10:02:26 +0000

    Created file for RadioDial

---
+++ frontend/src/components/RadioDial.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {ref, onMounted, onUnmounted} from 'vue';
+
+const props = defineProps({
+    modelValue: { type : Number, required: true},
+    min: { type : Number, default: 88.0},
+    max: { type : Number, default: 108.0},
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+</script>
